0d3057f390c3dd06257badb7939c73cffb684201
svr <svr.work@protonmail.com>
2022-12-06 19:48:48 +0300

    initial commit

---
+++ tgbot.py
@@ -0,0 +1,16 @@
+#!/bin/env python3
+
+import asyncio
+import telegram
+
+async def main():
+    token = ""
+    with open('token.txt') as f:
+        token = f.readline()
+    print("token {t}\n".format(t = token))
+    bot = telegram.Bot(token)
+    async with bot:
+        print(await bot.get_me())
+
+if __name__ == '__main__':
+    asyncio.run(main())

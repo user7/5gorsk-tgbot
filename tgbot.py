00f1d2491dfcb41b123a8267956fd5f5a0172351
svr <svr.work@protonmail.com>
2022-12-06 20:45:08 +0300

    wip

--- tgbot.py
+++ tgbot.py
@@ -6,8 +6,8 @@ import telegram
 async def main():
     token = ""
     with open('token.txt') as f:
-        token = f.readline()
-    print("token {t}\n".format(t = token))
+        token = f.readline().rstrip()
+    print("token {t}".format(t = token))
     bot = telegram.Bot(token)
     async with bot:
         print(await bot.get_me())

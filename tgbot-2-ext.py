3e91939e9eb7aafb3619728f2d14143d9faf0a6c
svr <svr.work@protonmail.com>
2022-12-13 19:34:17 +0300

    echo bot

--- tgbot-2-ext.py
+++ tgbot-2-ext.py
@@ -1,6 +1,10 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters
+from telegram.ext import MessageHandler
+from telegram.ext import ApplicationBuilder
+from telegram.ext import ContextTypes
+from telegram.ext import CommandHandler
 
 logging.basicConfig(
     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -10,11 +14,16 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id = update.effective_chat.id, text = "I'm a bot, talk!")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id = update.effective_chat.id, text = update.message.text)
+
 if __name__ == '__main__':
     with open('token.txt') as f:
         token = f.readline().rstrip()
     application = ApplicationBuilder().token(token).build()
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(start_handler)
+    application.add_handler(echo_handler)
     application.run_polling()
 
